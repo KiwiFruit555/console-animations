7bb3336176eefcf1f83921b9535b34e8b29c308c
KiwiFruit555 <64765206+KiwiFruit555@users.noreply.github.com>
2021-04-23 21:10:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 
 int loading() {
-  sleep(1);
+  sleep(1000);
   cout << "Loading... ";
-  sleep(1);
+  sleep(1000);
   cout << "hi";
-  sleep(1);
+  sleep(1000);
   cout << "e";
-  sleep(1);
+  sleep(1000);
   return 0;
 }
 int main() {
